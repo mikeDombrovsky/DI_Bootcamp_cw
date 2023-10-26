aabc9c8188545722264c1cf94047cad94ffe1042
Mikhail Dombrovsky <madombrovsky@gmail.com>
2023-10-26 16:44:03 +0300

    react robofriends2

--- week13-day1-3/robofriends/src/App.js
+++ week13-day1-3/robofriends/src/App.js
@@ -1,21 +1,35 @@
-import Card from './Card'
+import SearchBox from './SearchBox'
+import CardList from './CardList';
 import { robots } from './robots';
+import React, { Component } from 'react';
+import './App.css';
 
-function App() {
-  return (
-    <>
-      {robots.map(robot => {
-        return (
-          <Card
-            id={robot.id}
-            name={robot.name}
-            username={robot.username}
-            email={robot.email}
-          />
-        )
-      })}
-    </>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchField: ''
+    }
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value });
+  }
+
+  render() {
+    const filterRobots = this.state.robots.filter(robot => {
+      return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+    });
+
+    return (
+      <div className='tc'>
+        <h1 className='f1'>Robofriends</h1>
+        <SearchBox seachChange={this.onSearchChange} />
+        <CardList robots={filterRobots} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ week13-day1-3/robofriends/src/CardList.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Card from "./Card";
+
+const CardList = ({ robots }) => {
+    return (
+        <>
+            {
+                robots.map(robot => {
+                    return (
+                        <Card
+                            key={robot.id}
+                            id={robot.id}
+                            name={robot.name}
+                            username={robot.username}
+                            email={robot.email}
+                        />
+                    )
+                })
+            }
+        </>
+    );
+}
+
+export default CardList;
