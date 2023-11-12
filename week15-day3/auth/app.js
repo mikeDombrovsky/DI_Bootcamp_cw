2fb6173c4ec95b047a8b114e5b8aac190380101b
Mikhail Dombrovsky <madombrovsky@gmail.com>
2023-11-12 23:50:03 +0200

    week 15 day3

--- week15-day3/auth/app.js
+++ week15-day3/auth/app.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/users", users_router);
 
--- week15-day3/auth/auth.js
+++ week15-day3/auth/auth.js
@@ -1,16 +1,16 @@
-// const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 
-// //create token
-// const token = jwt.sign({ username: "me", uid: 1 }, "secret12345", {
-//   expiresIn: "180s",
-// });
+//create token
+const token = jwt.sign({ username: "me", uid: 1 }, "secret12345", {
+  expiresIn: "180s",
+});
 
-// console.log(token);
+console.log(token);
 
-// const oldToken =
-//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im1lIiwidWlkIjoxLCJpYXQiOjE2OTkzODA5MTAsImV4cCI6MTY5OTM4MTA5MH0.UiuCqum34Vw7JFlXtUdPoUde6kww6OoZCKPIwBQlEBg";
+const oldToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im1lIiwidWlkIjoxLCJpYXQiOjE2OTkzODA5MTAsImV4cCI6MTY5OTM4MTA5MH0.UiuCqum34Vw7JFlXtUdPoUde6kww6OoZCKPIwBQlEBg";
 
-// jwt.verify(oldToken, "secret12345", (err, decode) => {
-//   if (err) return console.log(err);
-//   console.log(decode);
-// });
+jwt.verify(oldToken, "secret12345", (err, decode) => {
+  if (err) return console.log(err);
+  console.log(decode);
+});
