0526f8e2e1ae08285c08a5367b7fa00279723d1d
Mikhail Dombrovsky <madombrovsky@gmail.com>
2023-12-17 13:35:15 +0200

    week19 day5 remote learning

--- week19-day1/sets.py
+++ week19-day1/sets.py
@@ -1,9 +1,10 @@
 a = {1, 2, 3, 4, 5}
 b = {2, 3, 6, 7, 5}
-c = a ^ b # add to new list all from b to a, without duplicates from a and b(minus join)
+# add to new list all from b to a, without duplicates from a and b(minus join)
+c = a ^ b
 print('a =', a, 'b =', b)
-print('a ^ b = ',c)
-d = a - b # remove 
+print('a ^ b = ', c)
+d = a - b  # remove
 print('a - b = ', d)
-e = b - a 
+e = b - a
 print('b - a = ', e)
---
+++ week19-day5/practice.py
@@ -0,0 +1,29 @@
+list_with_duplicates = ['a', 'b', 'c', 'b', 'm', 'n', 'n']
+
+duplicates = []
+for elem in list_with_duplicates:
+    if list_with_duplicates.count(elem) >= 2:
+        if duplicates.count(elem) == 0:
+            duplicates.append(elem)
+
+print(duplicates)
+
+# Exercise! Display the image below to the right hand side where the 0 is going to be ' ', and the 1 is going to be
+# '*'. This will reveal an image!
+picture = [
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0],
+    [0, 0, 1, 1, 1, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 1, 0, 0, 0],
+]
+
+for row in picture:
+    for column_elem in row:
+        if column_elem == 0:
+            print(" ", end='')
+        elif column_elem == 1:
+            print("*", end='')
+    print('')
